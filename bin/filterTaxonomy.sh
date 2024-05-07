15989d9722488ea0ee9e2c249b19605d47b9b1b0
Lee Katz <gzu2@cdc.gov>
2024-05-07 14:02:30 -0400

    Building taxonomy (#38)
    * building taxonomy files but this script will be deprecated right away
    
    * deprecated
    
    * script to build taxonomy with src files
    
    * m
    
    * move old taxonomy to deprecated
    
    * remove old 'versioned' files outside of git versioning
    
    * filter taxonomy script
    
    * complete the taxonomy
    
    * updated scripts for compiling databases
    
    * dev branch testing
    
    * fix lmono test a bit
    
    * .

---
+++ bin/filterTaxonomy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eu
+
+thisdir=$(dirname $0)
+thisfile=$(basename $0)
+KALAMARI_VER=$(downloadKalamari.pl --version)
+
+# Set up some directories
+tempdir=$(mktemp -d $thisfile.XXXXXX)
+trap "rm -rf $tempdir" EXIT
+outdir="$thisdir/../share/kalamari-$KALAMARI_VER/taxonomy/filtered"
+srcdir="$thisdir/../share/kalamari-$KALAMARI_VER/taxonomy"
+mkdir -pv $outdir
+
+# output files
+outnodes="$outdir/nodes.dmp"
+outnames="$outdir/names.dmp"
+
+# source taxonomy
+srcnodes="$srcdir/nodes.dmp"
+srcnames="$srcdir/names.dmp"
+
+# source leaf taxids
+taxid=$(cut -f 3,4 $thisdir/../src/chromosomes.tsv $thisdir/../src/plasmids.tsv | grep -v taxid | tr '\t' '\n' | sort -n | uniq)
+
+# Getting all necessary taxids
+alltaxids=$(echo "$taxid" | taxonkit --data-dir=$srcdir lineage -t | cut -f 3 | tr ';' '\n' | grep . | sort -n | uniq)
+numtaxids=$(wc -c <<< $alltaxids)
+echo "found $numtaxids taxids after calculating each taxon's lineage"
+
+# Filter nodes.dmp and names.dmp for $alltaxids
+echo "Finding all filtered taxids in $srcnodes"
+num=0
+# Replace the for loop with regex for grep
+regex=$(echo "$alltaxids" | perl -plane 's/(\d+)/^$1\t/' | tr '\n' '|' | sed 's/|$//');
+
+grep -E "$regex" $srcnodes > $outnodes
+grep -E "$regex" $srcnames > $outnames
+
+# Copy in the rest of the source files
+echo "Copying any remaining taxonomy files to the target"
+for i in $srcdir/*.dmp; do
+    cp -nv $i $outdir/
+done
